03f8b4e7fb02d514ee54db37eeb0465d303e1d05
gartcimore <gart.algar@gmail.com>
2019-12-17 17:23:55 +0100

    add steps in sample to insert data and query it back

--- samples/src/main/java/sample/App.java
+++ samples/src/main/java/sample/App.java
@@ -6,6 +6,8 @@ package sample;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
 
@@ -17,33 +19,67 @@ import java.time.Duration;
 
 public class App {
 
+  CqlSession session;
   private Logger logger = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
+    App app = new App();
     try {
-      new App().connect();
+      app.connect();
+      //app.createDataStructure();
+      app.insertData();
+      app.queryData();
+
     } catch (Exception e) {
       e.printStackTrace();
+    } finally {
+      app.close();
     }
   }
 
-  public synchronized Boolean connect() {
-    logger.info("connecting to cassandra");
+  private void close() {
+    if (!session.isClosed()) {
+      session.close();
+    }
+  }
 
-    DriverConfigLoader loader = DriverConfigLoader.programmaticBuilder()
-      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(5))
-      .startProfile("slow")
-      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(30))
-      .endProfile()
-      .build();
+  private void insertData() {
+    if (!session.isClosed()) {
+      try {
+        logger.info("inserting data");
+        session.execute("INSERT INTO genesys.emails (id, subject, body, userid)\n"
+          + "    VALUES (904b88b2-9c61-4539-952e-c179a3805b22, 'Hello world', 'Cassandra is great, "
+          + "but it''s even better with EsIndex and Elasticsearch', 42);");
+        logger.info("data inserted");
+      } catch (Exception e) {
+        session.close();
+        logger.warn("Something went wrong", e);
+      }
+    }
 
-    CqlSession session = CqlSession.builder().withConfigLoader(loader)
-      .addContactPoint(new InetSocketAddress("localhost", 9041))
-      .withLocalDatacenter("datacenter1")
-      .build();
+  }
 
+
+
+  private void queryData() {
+    if (!session.isClosed()) {
+      try {
+        logger.info("query data in elastic search through cassandra (query='body:cassan*';)");
+        final ResultSet resultSet =
+          session.execute("select id, subject, body, userid, query  from genesys.emails where query='body:cassan*';");
+        for (Row row : resultSet) {
+          logger.info(row.getFormattedContents());
+        }
+      } catch (Exception e) {
+        session.close();
+        logger.warn("Something went wrong", e);
+      }
+    }
+
+  }
+
+  private void createDataStructure() {
     try {
-      logger.info("connected");
       CreateKeyspace createKs = SchemaBuilder.createKeyspace("genesys").ifNotExists().withSimpleStrategy(1);
       session.execute(createKs.build());
 
@@ -65,10 +101,26 @@ public class App {
     } catch (Exception e) {
       session.close();
       logger.warn("Something went wrong", e);
-      return false;
     }
-    logger.info("all done");
+    logger.info("all created");
 
+  }
+
+  public synchronized Boolean connect() {
+    logger.info("connecting to cassandra");
+
+    DriverConfigLoader loader = DriverConfigLoader.programmaticBuilder()
+      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(5))
+      .startProfile("slow")
+      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(30))
+      .endProfile()
+      .build();
+
+    session = CqlSession.builder().withConfigLoader(loader)
+      .addContactPoint(new InetSocketAddress("localhost", 9041))
+      .withLocalDatacenter("datacenter1")
+      .build();
+    logger.info("connected");
     return true;
   }
 }
